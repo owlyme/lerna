807e3e21d7201b3ed4726bfd15a2baf619af22f9
yuan.xu01@weimob.com <yuan.xu01>
2021-03-23 19:52:21 +0800

    first

---
+++ packages/lerna-owlyme-popular/index.js
@@ -0,0 +1,11 @@
+const { API } = require('lerna-owlyme-core');
+const axios = require('axios');
+
+const getPopularImg = () => axios.get(API)
+
+module.exports = getPopularImg;
+
+// 测试代码，发布时删除
+getPopularImg().then((res) => console.log(res.data.length));
+
+// lerna add prpr-lerna-core --scope=prpr-lerna-popular
