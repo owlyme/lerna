a810d6c4263268bd9c3fd1d42bd8f33f56e12c6c
yuan.xu01@weimob.com <yuan.xu01>
2021-03-24 10:00:01 +0800

    ..

---
+++ packages/CheckBox/rollup_config.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        input: 'src/index.js',
+        fileName: "CheckBox",
+        compnentName: "WeiMob.CheckBox",
+        outPutBasePath: "./dist",
+        external: ['react', 'loadsh', './B1'],
+    }
+]
